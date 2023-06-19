7e26822184e8d2378f1d189e92a24834ed921d32
JukioNT <jjulio2468@gmail.com>
2023-06-18 22:37:13 -0300

    Noticias slider

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -4,26 +4,35 @@
     <div class="album py-5">
         <div class="container">
             <h1>Noticias</h1>
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                @foreach ($noticia as $item => $value)
-                    <div class="col">
-                        <div class="card shadow-sm">
-                            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="/storage/{{$value->imagem}}">
-                        </div>
-                        <div class="card-body">
-                            <h3 class="py-3">{{$value['titulo']}}</h3>
-                            <p class="card-text">{{$value['descricao']}}</p>
-                            @if(Auth::check())
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <a href="/noticias/editar/{{$value['id']}}" type="button" class="btn btn-sm btn-outline-secondary">Editar</a>
-                                    <a onclick="return confirm('Tem certeza que deseja deletar?')" href="/noticias/deletar/{{$value['id']}}" type="button" class="btn btn-sm btn-outline-secondary" >Deletar</a>
-                                </div>
+            <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="false">
+                <div class="carousel-indicators">
+                    @foreach ($noticia as $index => $item)
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="{{ $index }}"
+                            @if ($index == 0) class="active" aria-current="true" @endif
+                            aria-label={{ 'Slide' . ($index + 1) }}></button>
+                    @endforeach
+                </div>
+                <div class="carousel-inner">
+                    @foreach ($noticia as $item => $value)
+                        <div data-bs-interval="10" @if ($item == 0) class="carousel-item active" @else class="carousel-item" @endif>
+                            <img src="/storage/{{ $value->imagem }}" alt="" class="d-block w-100">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>{{ $value['titulo'] }}</h5>
+                                <p>{{ $value['descricao'] }}</p>
                             </div>
-                            @endif
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
             </div>
         </div>
         <div class="container">
@@ -32,32 +41,25 @@
                 @foreach ($evento as $item => $value)
                     <div class="col">
                         <div class="card shadow-sm">
-                            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="/storage/{{$value->imagem}}">
+                            <img class="bd-placeholder-img card-img-top" width="100%" height="225"
+                                src="/storage/{{ $value->imagem }}">
                         </div>
                         <div class="card-body">
-                            <h3 class="py-3">{{$value['titulo']}}</h3>
-                            <p class="card-text">{{$value['descricao']}}</p>
+                            <h3 class="py-3">{{ $value['titulo'] }}</h3>
+                            <p class="card-text">{{ $value['descricao'] }}</p>
                             <div class="d-flex justify-content-between align-items-center">
-                                @if(Auth::check())
-                                <div class="btn-group">
-                                    <a href="/eventos/editar/{{$value['id']}}" type="button" class="btn btn-sm btn-outline-secondary">Editar</a>
-                                    <a onclick="return confirm('Tem certeza que deseja deletar?')" href="/eventos/deletar/{{$value['id']}}" type="button" class="btn btn-sm btn-outline-secondary" >Deletar</a>
-                                    
-                                </div>
+                                @if (Auth::check())
+                                    <div class="btn-group">
+                                        <a href="/eventos/editar/{{ $value['id'] }}" type="button"
+                                            class="btn btn-sm btn-outline-secondary">Editar</a>
+                                        <a onclick="return confirm('Tem certeza que deseja deletar?')"
+                                            href="/eventos/deletar/{{ $value['id'] }}" type="button"
+                                            class="btn btn-sm btn-outline-secondary">Deletar</a>
+
+                                    </div>
                                 @endif
-                                <small>Data do evento: {{date('d/m/Y H:i', strtotime($value['dataEvento']))}}</small>
+                                <small>Data do evento: {{ date('d/m/Y H:i', strtotime($value['dataEvento'])) }}</small>
                             </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-        </div>
-        <div class="container">
-            <div class="scroll-container">
-                @foreach ($noticia as $item => $value)
-                    <div class="col">
-                        <div class="card shadow-sm">
-                            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="/storage/{{$value->imagem}}">
                         </div>
                     </div>
                 @endforeach
