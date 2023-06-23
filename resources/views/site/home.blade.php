4af044f8c74698e604d1fc91b0d6f6b13ae59ef5
JukioNT <jjulio2468@gmail.com>
2023-06-23 15:31:39 -0300

    Changed arrow colors

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -12,7 +12,7 @@
                             @foreach ($noticia as $index => $item)
                                 <button type="button" data-bs-target="#carouselExampleCaptions"
                                     data-bs-slide-to="{{ $index }}"
-                                    @if ($index == 0) class="active" aria-current="true" @endif
+                                    @if ($index == 0) class="active noticia-button" aria-current="true" @else class="noticia-button" @endif
                                     aria-label={{ 'Slide' . ($index + 1) }}></button>
                             @endforeach
                         </div>
@@ -21,9 +21,7 @@
                         @foreach ($noticia as $item => $value)
                             <div data-bs-interval="10"
                                 @if ($item == 0) class="carousel-item active" @else class="carousel-item" @endif>
-                                <div class="vignette">
-                                    <img src="/storage/{{ $value->imagem }}" alt="" class="d-block w-100 noticia-img">
-                                </div>
+                                <img src="/storage/{{ $value->imagem }}" alt="" class="d-block w-100 noticia-img">
                                 <div class="carousel-caption d-none d-md-block">
                                     <h5>{{ $value['titulo'] }}</h5>
                                     <p>{{ $value['descricao'] }}</p>
@@ -34,12 +32,12 @@
                     @if (count($noticia) != 1)
                         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
                             data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="carousel-control-prev-icon noticia-left-arrow" aria-hidden="true" ></span>
                             <span class="visually-hidden">Previous</span>
                         </button>
                         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
                             data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="carousel-control-next-icon noticia-right-arrow" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                         </button>
                     @endif
