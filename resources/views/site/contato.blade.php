f7a5410b91248934d7fa738f09f0b36d058fcdbc
JukioNT <jjulio2468@gmail.com>
2023-10-16 15:56:12 -0300

    Sobre && Contatos page

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,8 +1,94 @@
 @extends('site.layout')
-@section('title', 'Sobre nós')
+@section('title', 'Contato')
 @section('body')
 	<div class="info" id="sobrenos">
         <h1>Contato</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil excepturi corrupti molestiae perspiciatis error nemo quod sint quasi! Laudantium itaque laboriosam aut aperiam et saepe tempore suscipit maxime sunt hic, in ad molestiae, quibusdam, odit eius qui quidem nemo perferendis distinctio quos ipsam! Unde, quidem dolorem. Dolores officiis architecto sit nisi temporibus at corrupti, ut, vero qui consequatur, maiores beatae quasi ipsam omnis. Sequi, culpa quaerat assumenda soluta explicabo commodi blanditiis. Unde maiores voluptatem tenetur debitis! Saepe sit velit asperiores blanditiis sint, consectetur molestiae maiores reprehenderit mollitia obcaecati eius corporis soluta facere cupiditate in quasi? Dolore maiores hic eveniet! Velit!</p>
+        <div class="sub">
+            <p>
+                Caso tenha alguma dúvida, sinta-se à vontade para nos contatar pelos seguintes canais:
+            </p>
+            <div class="sub">
+                <ul>
+                    <li>E-mail: redescasadocaminho@gmail.com</li>
+                    <li>Instagram: <a href="http://www.instagram.com/casadocaminhotp" target="_blank">http://www.instagram.com/casadocaminhotp</a></li>
+                    <li>Whatsapp: <a href="http://chat.whatsapp.com/EP0trSySaU22lXPsqbpvik" target="_blank">http://chat.whatsapp.com/EP0trSySaU22lXPsqbpvik</a></li>
+                </ul>
+            </div>
+            <p>
+                Através desses meios, você pode se comunicar com os voluntários responsáveis conforme o tema ou a necessidade:
+            </p>
+            <div class="sub">
+                <p>
+                    Atendimento fraterno: Eliane | Maria Flávia (agendamento).
+                </p>
+            </div>
+            <p>
+                O Atendimento fraterno é um serviço oferecido a pessoas que buscam elucidações espíritas para os seus problemas íntimos, dificuldades existenciais, conflitos e anseios.
+            </p>
+            <p>
+                Funcionamento e dúvidas gerais:
+            </p>
+            <div class="sub">
+                <ul>
+                    <li>Eliane</li>
+                    <li>Wilma</li>
+                    <li>Hike</li>
+                    <li>Maria Flávia</li>
+                </ul>
+            </div>
+            <p>
+                Dúvidas doutrinárias:
+            </p>
+            <div class="sub">
+                <ul>
+                    <li>Eliane</li>
+                    <li>Hike</li>
+                    <li>Maria Flávia</li>
+                </ul>
+            </div>
+            <p>
+                Voluntariado:
+            </p>
+            <div class="sub">
+                <ul>
+                    <li>Maria Flávia</li>
+                    <li>Wilma</li>
+                    <li>Hike</li>
+                    <li>Eliane</li>
+                </ul>
+            </div>
+            <p>Brechó:</p>
+            <div class="sub">
+                <ul>
+                    <li>Wilma</li>
+                    <li>Maria Flávia</li>
+                </ul>
+            </div>
+            <p>
+                Endereço loja:
+            </p>
+            <div class="sub">
+                <p>
+                    Av Ipiranga 52 - Centro <br>
+                    Terça | Quinta | Sexta das 14h30 às 18h.
+                </p>
+            </div>
+            <p>
+                Endereço doações
+            </p>
+            <div class="sub">
+                <p>
+                    Rua Benjamim da Silva Campos 172 - Vila Campos. <br>
+                    Segunda a Sexta das 08h às 14h.
+                </p>
+            </div>
+            <p>
+                Doações:
+                <div class="sub">
+                    <p>37627021000141 (Pix | CNPJ)</p>
+                </div>
+            </p>
+            <p>Ficamos à disposição para esclarecer suas dúvidas e compartilhar informações com você!</p>
+        </div>
     </div>
 @endsection
--- resources/views/site/sobreNos.blade.php
+++ resources/views/site/sobreNos.blade.php
@@ -3,6 +3,34 @@
 @section('body')
 	<div class="info" id="sobrenos">
         <h1>Sobre Nós</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil excepturi corrupti molestiae perspiciatis error nemo quod sint quasi! Laudantium itaque laboriosam aut aperiam et saepe tempore suscipit maxime sunt hic, in ad molestiae, quibusdam, odit eius qui quidem nemo perferendis distinctio quos ipsam! Unde, quidem dolorem. Dolores officiis architecto sit nisi temporibus at corrupti, ut, vero qui consequatur, maiores beatae quasi ipsam omnis. Sequi, culpa quaerat assumenda soluta explicabo commodi blanditiis. Unde maiores voluptatem tenetur debitis! Saepe sit velit asperiores blanditiis sint, consectetur molestiae maiores reprehenderit mollitia obcaecati eius corporis soluta facere cupiditate in quasi? Dolore maiores hic eveniet! Velit!</p>
+        <div class="sub">
+            <h3>Bem-vindos ao Centro Espírita Casa do Caminho de Três Pontas!</h3>
+            <div class="sub">
+                <p>
+                    É com grande alegria que compartilhamos nossa jornada no universo do Espiritismo. Fundado em 03 de outubro de 2019, somos uma comunidade unida pelo desejo de estudar, praticar e difundir os princípios da Doutrina Espírita, baseados nas obras fundamentais de Allan Kardec e recomendações da Federação Espírita Brasileira - FEB. 
+                </p>
+                <p>
+                    Esse propósito vai além do estudo teórico; buscamos colocar em prática a caridade em seus diversos aspectos: moral, material e espiritual. Acreditamos no poder transformador do amor ao próximo e nos empenhamos em desenvolver atividades beneficentes, culturais, filantrópicas e assistenciais em nossa comunidade.
+                </p>
+                <p>
+                    O Centro Espírita Casa do Caminho, está aberto para todos que desejam conhecer, aprender e crescer espiritualmente, sem  barreiras ou preconceitos. Valorizamos a diversidade e acolhemos todos, independentemente de raça, sexo, cor ou religião. Nosso lema é o amor, princípio que nos guia em cada ação que empreendemos.
+                </p>
+                <p>
+                    Dispomos de uma biblioteca repleta de conhecimento, com espaço para estudo e reflexão. Além disso, promovemos reuniões públicas, cursos e atividades doutrinárias que visam expandir a compreensão do Espiritismo e suas nuances.
+                </p>
+                <p>
+                    Nossos colaboradores, sejam efetivos ou eventuais, são a parte vital de nossa comunidade, contribuindo para a realização dos objetivos e a promoção do bem em nossa sociedade.
+                </p>
+                <p>
+                    Acreditamos no poder da união e, por isso, mantemos vínculos estreitos com outras sociedades espíritas, visando unificar esforços para um mundo melhor. Juntos, somos mais fortes e capazes de espalhar a luz do Espiritismo.
+                </p>
+                <p>
+                    Convidamos você a explorar, questionar, aprender e crescer conosco. Vamos juntos trilhar esse caminho de amor e evolução, impactando positivamente nossa comunidade.
+                </p>
+                <p>
+                    Receba, então, nosso convite para conhecer e fazer parte desta Casa do Caminho, onde a luz do conhecimento e do amor nos guia a cada passo, como nos lembram Chico Xavier e Emmanuel: "A caridade é o processo de somar alegrias, diminuir males, multiplicar esperanças e dividir a felicidade para que a Terra se realize na condição do esperado Reino de Deus".
+                </p>
+            </div>
+        </div>
     </div>
 @endsection
