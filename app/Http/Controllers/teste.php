5a12e86e4c0d098d1e2e92256e4698315e391f8f
JukioNT <jjulio2468@gmail.com>
2023-10-23 21:36:52 -0300

    Images save start fix

--- app/Http/Controllers/teste.php
+++ app/Http/Controllers/teste.php
@@ -1,64 +1,13 @@
 <?php
+    function teste($path){
+        $origem = "../../../storage/app/public/".$path;
+        echo $origem;
 
-namespace App\Http\Controllers;
+        $destino = "../../../public/images/teste";
+        echo $destino;
 
-use Illuminate\Http\Request;
-
-class teste extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+        if(!copy($origem, $destino)){
+            echo "Falha ao copiar a imagem";
+        }
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
-}
+?>
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind('path.public', function() {
-            return base_path().'/public_html';
-        });        
+            
     }
 
     /**
